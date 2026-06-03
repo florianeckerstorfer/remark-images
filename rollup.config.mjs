30f6ebebf53bb10e94011e53f52ba75298f6504c
Florian Eckerstorfer <florian@eckerstorfer.net>
2026-06-03 22:53:38 +0200

    chore: remove commonjs support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,16 @@
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { createRequire } from 'module';
-const pkg = createRequire(import.meta.url)('./package.json');
 
-export default [
-  {
-    input: 'src/index.js',
-    external: [
-      'sharp',
-      'probe-image-size',
-      'unist-util-visit-parents',
-      'fs',
-      'path',
-      'debug',
-    ],
-    output: [
-      { file: pkg.main, format: 'cjs', exports: 'default' },
-      { file: pkg.module, format: 'esm' },
-    ],
-    plugins: [json(), nodeResolve()],
-  },
-];
+export default {
+  input: 'src/index.js',
+  external: [
+    'sharp',
+    'probe-image-size',
+    'unist-util-visit-parents',
+    'fs',
+    'path',
+    'debug',
+  ],
+  output: { file: 'dist/remark-images.js', format: 'esm' },
+  plugins: [json(), nodeResolve()],
+};
